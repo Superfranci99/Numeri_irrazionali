87bb08ed05c342788efcd99d541d6b7e53f5d5bf
Superfranci99 <superfranci99@gmail.com>
2016-03-23 20:59:20 +0100

    Implementato lo scheletro PiAlgo

---
+++ Pigreco/Pigreco/PiAlgo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pigreco
+{
+    abstract class PiAlgo
+    {
+        /// <summary>
+        /// Numero di cifre da calcolare dopo la virgola
+        /// </summary>
+        public int Precisione
+        {
+            get { return Precisione; }
+            set { if (value > 0) Precisione = value; else Precisione = 1; }
+        }
+
+        public PiAlgo(int precisione)
+        {
+            this.Precisione = precisione;
+        }
+
+        /// <summary>
+        /// Calcola il valore di pi greco
+        /// </summary>
+        /// <returns></returns>
+        public abstract decimal Calcola();
+
+    }
+}
