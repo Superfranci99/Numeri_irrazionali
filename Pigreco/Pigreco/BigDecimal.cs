c4115fa86e210fa7a6cf8f952002763769b0db2e
Superfranci99 <superfranci99@gmail.com>
2016-04-03 10:37:36 +0200

    Conversione da BigDecimal a string

--- Pigreco/Pigreco/BigDecimal.cs
+++ Pigreco/Pigreco/BigDecimal.cs
@@ -23,15 +23,24 @@ namespace Pigreco
             if (div.Length > 2) throw new ArgumentException("Stringa non convertibile");
 
             // ottiene la parte intera del numero
-            this.ParteIntera = int.Parse(div[0]); 
+            ParteIntera = int.Parse(div[0]); 
 
             // ora la parte decimale...
             if (div[1].Length % 2 == 1) div[1] += "0"; // rende le cifre pari
             for(int i=0; i<div[1].Length; i += 2)
             {
                 string num = div[1].Substring(i, 2);
-                this.ParteDecimale.Add(int.Parse(num));
+                ParteDecimale.Add(int.Parse(num));
             }
+        }
+
+        public override string ToString()
+        {
+            string result = this.ParteIntera.ToString();
+            foreach (int n in ParteDecimale)
+                result += n.ToString();
+            result = result.TrimEnd('0');
+            return result;
         }
     }
 }
